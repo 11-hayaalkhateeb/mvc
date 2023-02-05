d8c6d65950668878002eb58ca950befa30d6227c
11-hayaalkhateeb <115453315+11-hayaalkhateeb@users.noreply.github.com>
2023-02-05 22:59:50 +0300

    task5-2

---
+++ mvctask5_2/mvctask5_2/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mvctask5_2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+        //public ActionResult Upload()
+        //{
+        //    return View();
+        //}
+
+        //[HttpPost]
+        //public ActionResult Upload(HttpPostedFileBase file)
+        //{
+        //    if (file != null && file.ContentLength > 0)
+        //    {
+        //        string fileName = Path.GetFileName(file.FileName);
+        //        string path = Path.Combine(Server.MapPath("~/uploads"), fileName);
+        //        file.SaveAs(path);
+        //    }
+
+        //    return RedirectToAction("");
+        //}
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ mvctask5_2/mvctask5_2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="mvctask5_2.MvcApplication" Language="C#" %>
